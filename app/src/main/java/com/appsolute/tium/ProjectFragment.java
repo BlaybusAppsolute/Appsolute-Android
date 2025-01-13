ead14408eacea62adafd0a3f655c239869d18eea
withthemoonrabbit <with.the.moon.rabbit@gmail.com>
2025-01-13 22:08:27 +0900

    first commit

--- app/src/main/java/com/appsolute/tium/ProjectFragment.java
+++ app/src/main/java/com/appsolute/tium/ProjectFragment.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectFragment extends Fragment {
-
+    //test
     private RecyclerView recyclerView;
     private ProjectAdapter adapter;
 
