176da836ef435c7c4ef569c9e9ee3ab9140763b5
withthemoonrabbit <with.the.moon.rabbit@gmail.com>
2025-01-14 19:09:23 +0900

    third commit

--- app/src/main/java/com/appsolute/tium/EditProfile.java
+++ app/src/main/java/com/appsolute/tium/EditProfile.java
@@ -1,6 +1,8 @@
 package com.appsolute.tium;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,15 @@ public class EditProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_edit_profile);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Add click listener for imageView11 to go back
+        ImageView backButton = findViewById(R.id.imageView11);
+        backButton.setOnClickListener(view -> finish()); // Closes the current activity and returns to HomeFragment
     }
 }
